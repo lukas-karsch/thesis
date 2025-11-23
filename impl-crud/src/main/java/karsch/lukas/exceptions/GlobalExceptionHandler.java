87c5b9cf6198c90e746f248c106275152409622e
Lukas <lk224@hdm-stuttgart.de>
2025-11-23 15:47:58 +0100

    feat(impl-crud): add default error cases to GlobalExceptionHandler

--- impl-crud/src/main/java/karsch/lukas/exceptions/GlobalExceptionHandler.java
+++ impl-crud/src/main/java/karsch/lukas/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package karsch.lukas.exceptions;
 
 import karsch.lukas.response.ApiResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,6 +14,20 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ApiResponse<Void>> handleResponseStatusException(ResponseStatusException ex) {
         return new ResponseEntity<>(
                 new ApiResponse<>(ex.getStatusCode(), ex.getMessage()), ex.getStatusCode()
+        );
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Void>> handleException(Exception ex) {
+        return new ResponseEntity<>(
+                new ApiResponse<>(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR
+        );
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ApiResponse<Void>> handleException(RuntimeException ex) {
+        return new ResponseEntity<>(
+                new ApiResponse<>(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR
         );
     }
 }
