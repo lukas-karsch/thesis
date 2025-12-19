4515c69af63b2fc2b371a5a36a5f4529141e032b
Lukas <lk224@hdm-stuttgart.de>
2025-12-19 16:37:55 +0100

    chore(es-cqrs): remove log statement

--- impl-es-cqrs/src/test/java/karsch/lukas/AxonTestcontainerConfiguration.java
+++ impl-es-cqrs/src/test/java/karsch/lukas/AxonTestcontainerConfiguration.java
@@ -1,7 +1,6 @@
 package karsch.lukas;
 
 import org.axonframework.test.server.AxonServerContainer;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
@@ -17,8 +16,6 @@ public class AxonTestcontainerConfiguration {
     @ServiceConnection
     @RestartScope
     AxonServerContainer axonServerContainer() {
-        var logger = LoggerFactory.getLogger(AxonTestcontainerConfiguration.class);
-        logger.info("axonServerContainer bean was called");
         return new AxonServerContainer(
                 DockerImageName.parse("axoniq/axonserver:latest")
         )
